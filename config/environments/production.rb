4421af7467f063d37a47279cb170c7770476ab70
Nicholas Moy <nicksmoy@gmail.com>
2020-01-28 12:46:47 -0500

    changed product asset variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,12 +27,12 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
-  
+
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'

8d94494719cfff0ca4f2ba50da1805beec053998
Nicholas Moy <nicksmoy@gmail.com>
2020-01-28 13:03:45 -0500

    temp comment out for quick seeding

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,6 @@
 class Place < ApplicationRecord
-  geocoded_by :address
-  after_validation :geocode, if: :address_changed?
+  # geocoded_by :address
+  # after_validation :geocode, if: :address_changed?
 
   has_many :crawl_places
   has_many :crawls, through: :crawl_places
